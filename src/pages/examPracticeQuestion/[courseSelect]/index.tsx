26dcbf0087c5ab965ad376218238e93a3090e032
Lukman <abdulrauflukman9@gmail.com>
2024-06-22 20:51:04 +0100

    create the courseCard component

--- src/pages/examPracticeQuestion/[courseSelect]/index.tsx
+++ src/pages/examPracticeQuestion/[courseSelect]/index.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/header";
+import Card from "@/components/rightDashboard/card";
 import { useRouter } from "next/router";
 import React from "react";
 
@@ -9,6 +10,28 @@ export default function CourseSelect() {
   return (
     <div>
       <Header title={titleFromQuery} fromQuery={Boolean(titleFromQuery)} />
+
+      <div className="mx-auto flex items-center justify-between rounded-md bg-white p-3">
+        <input
+          type="text"
+          placeholder="Search for courses"
+          className="rounded-md bg-[#F8F9FB] p-2"
+        />
+        <div className="flex">
+          <div className="flex gap-2 rounded-l-md bg-[#D32D4426] p-2">
+            <p className="text-[#D32D44]">Total courses available:</p>
+            <p>12</p>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <p className="space-y-6 font-semibold">
+          Select a course to take the examination.
+        </p>
+
+        <Card titlePoints={"dddd"} subtitle={"fff"} />
+      </div>
     </div>
   );
 }
