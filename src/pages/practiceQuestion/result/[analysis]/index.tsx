0675db53677be4c0204a72ba65c6d206902e0831
Lukman10a <abdulrauflukman9@gmail.com>
2024-07-19 22:47:10 +0100

    update analysis

--- src/pages/practiceQuestion/result/[analysis]/index.tsx
+++ src/pages/practiceQuestion/result/[analysis]/index.tsx
@@ -1,8 +1,239 @@
+// import { useQueries, useQuery } from "@tanstack/react-query";
+// import { useRouter } from "next/router";
+// import { useState, useEffect, useMemo } from "react";
+// import { useSelector } from "react-redux";
+// import { RootState } from "@/store";
+// import BarChart from "@/components/barChat";
+// import Loading from "@/components/loading";
+// import { fetchStudentTests, fetchTestResult } from "@/components/utils";
+// import { IoNotificationsCircleOutline } from "react-icons/io5";
+// import { Test, TestResult } from "@/type/testResult";
+// import DatePicker from "react-datepicker";
+// import "react-datepicker/dist/react-datepicker.css";
+
+// export default function Analysis() {
+//   const { query } = useRouter();
+//   // console.log({ query: query });
+
+//   const [selectedTestId, setSelectedTestId] = useState<string | null>(null);
+//   const [selectedTopicName, setSelectedTopicName] = useState<string | null>(
+//     null,
+//   );
+//   const [dateRange, setDateRange] = useState<[Date | null, Date | null]>([
+//     null,
+//     null,
+//   ]);
+//   const [startDate, endDate] = dateRange;
+//   const [backgroundColor, setBackgroundColor] = useState<string>("#D32D441A");
+//   const [filteredChartData, setFilteredChartData] = useState<any>(null);
+//   const [topicName, setTopicName] = useState<{ id: string; name: string }[]>(
+//     [],
+//   );
+
+//   // const topicNames = useSelector((state: RootState) => state.quiz.topicNames);
+
+//   const {
+//     data: studentTests,
+//     isLoading: isLoadingTests,
+//     error: testsError,
+//   } = useQuery({
+//     queryKey: ["studentTests"],
+//     queryFn: fetchStudentTests,
+//     select: (tests: Test[]) =>
+//       tests.filter((item) => query.analysis === item.course_name),
+//   });
+
+//   const testResults = useQueries({
+//     queries: studentTests
+//       ? studentTests.map((test: Test) => ({
+//           queryKey: ["studentResult", test.id],
+//           queryFn: () => fetchTestResult(test.id),
+//         }))
+//       : [],
+//   });
+
+//   const flattenedTests: TestResult[] = testResults
+//     .flatMap((result) => result.data)
+//     .filter(Boolean);
+
+//   const chartData = {
+//     labels: flattenedTests.map((item) =>
+//       new Date(item.created_at).toLocaleDateString(),
+//     ),
+//     datasets: [
+//       {
+//         label: "Test Scores (%)",
+//         data: flattenedTests.map(
+//           (item) =>
+//             (parseFloat(item?.score) / item?.test?.question_ids?.length) * 100,
+//         ),
+//         backgroundColor: "#D32D44",
+//         borderColor: "rgba(54, 162, 235, 1)",
+//         borderWidth: 1,
+//         borderRadius: 10,
+//       },
+//     ],
+//   };
+
+//   const options = {
+//     scales: {
+//       y: {
+//         beginAtZero: true,
+//         max: 100,
+//         ticks: {
+//           callback: function (value: number) {
+//             return value + "%";
+//           },
+//         },
+//       },
+//     },
+//   };
+
+//   const handleTestIdChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+//     const selectedName = event.target.value;
+//     setSelectedTestId(selectedName);
+//     setSelectedTopicName(selectedName); // Set the selected topic name
+//   };
+
+//   const minDate = useMemo(() => {
+//     if (flattenedTests.length === 0) return null;
+//     return new Date(
+//       Math.min(
+//         ...flattenedTests.map((item) => new Date(item.created_at).getTime()),
+//       ),
+//     );
+//   }, [flattenedTests]);
+
+//   const maxDate = useMemo(() => {
+//     if (flattenedTests.length === 0) return null;
+//     return new Date(
+//       Math.max(
+//         ...flattenedTests.map((item) => new Date(item.created_at).getTime()),
+//       ),
+//     );
+//   }, [flattenedTests]);
+
+//   useEffect(() => {
+//     const fiveMinutesInMillis = 23 * 59 * 59 * 1000;
+
+//     if (selectedTopicName || (startDate && endDate)) {
+//       const filteredTests = flattenedTests
+//         .filter(
+//           (test) =>
+//             !selectedTopicName || test.test.topic_id === selectedTopicName,
+//         )
+//         .filter(
+//           (item: { created_at: string | number | Date }) =>
+//             (!startDate || new Date(item.created_at) >= startDate) &&
+//             (!endDate || new Date(item.created_at) <= endDate),
+//         );
+
+//       const filteredChartData = {
+//         labels: filteredTests.map((item) =>
+//           new Date(item.created_at).toLocaleDateString(),
+//         ),
+//         datasets: [
+//           {
+//             label: "Test Scores (%)",
+//             data: filteredTests.map(
+//               (item) =>
+//                 (parseFloat(item?.score) / item?.test?.question_ids?.length) *
+//                 100,
+//             ),
+//             backgroundColor: "#D32D44",
+//             borderColor: "rgba(54, 162, 235, 1)",
+//             borderWidth: 1,
+//           },
+//         ],
+//       };
+
+//       setFilteredChartData(filteredChartData);
+//     } else {
+//       // Reset filteredChartData when no filters are applied
+//       setFilteredChartData(null);
+//     }
+//   }, [selectedTopicName, startDate, endDate, flattenedTests]);
+
+//   useEffect(() => {
+//     if (studentTests) {
+//       const uniqueTopics = Array.from(
+//         new Set(studentTests.map((test) => test.topic_name)),
+//       ).map((topicName) => ({
+//         id: topicName,
+//         name: topicName,
+//       }));
+//       setTopicName(uniqueTopics);
+//     }
+//   }, [studentTests]);
+
+//   if (isLoadingTests) return <Loading />;
+//   if (testsError) return <p>An error has occurred: {testsError.message}</p>;
+
+//   return (
+//     <div>
+//       <div className="mb-3 flex items-center justify-between">
+//         <p className="font-dm_sans text-2xl font-medium">{query.analysis}</p>
+//         <div className="flex items-center gap-2">
+//           <IoNotificationsCircleOutline size={40} />
+//         </div>
+//       </div>
+
+//       <div className="mx-auto rounded-md bg-white p-4">
+//         <div className="flex items-center justify-between gap-2">
+//           <p>Performance analysis</p>
+//         </div>
+
+//         <div className="my-8 flex justify-between">
+//           <div className="flex gap-2">
+//             <select
+//               value={selectedTestId || ""}
+//               onChange={handleTestIdChange}
+//               className="flex items-center justify-between gap-12 rounded-md border-2 p-2 px-6"
+//             >
+//               <option value="">Select Topic</option>
+//               {topicName.map((name, index) => (
+//                 <option key={index} value={name.id}>
+//                   {name.name}
+//                 </option>
+//               ))}
+//             </select>
+//             <DatePicker
+//               selectsRange
+//               startDate={startDate ?? undefined}
+//               endDate={endDate ?? undefined}
+//               // allowSameDay={false}
+//               onChange={(update) => {
+//                 setDateRange(update);
+//               }}
+//               isClearable={true}
+//               minDate={minDate ?? undefined}
+//               maxDate={maxDate ?? undefined}
+//               className="flex items-center justify-between gap-12 rounded-md border-2 p-2 px-6"
+//             />
+//           </div>
+
+//           <div>
+//             <button className="button-2 rounded-md bg-[#D32D44] p-2 text-white">
+//               Download Analysis
+//             </button>
+//           </div>
+//         </div>
+
+//         {chartData && (
+//           <BarChart
+//             data={filteredChartData || chartData}
+//             backgroundColor={backgroundColor}
+//             options={options}
+//           />
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+
 import { useQueries, useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/router";
 import { useState, useEffect, useMemo } from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "@/store";
 import BarChart from "@/components/barChat";
 import Loading from "@/components/loading";
 import { fetchStudentTests, fetchTestResult } from "@/components/utils";
@@ -13,7 +244,6 @@ import "react-datepicker/dist/react-datepicker.css";
 
 export default function Analysis() {
   const { query } = useRouter();
-  // console.log({ query: query });
 
   const [selectedTestId, setSelectedTestId] = useState<string | null>(null);
   const [selectedTopicName, setSelectedTopicName] = useState<string | null>(
@@ -26,11 +256,7 @@ export default function Analysis() {
   const [startDate, endDate] = dateRange;
   const [backgroundColor, setBackgroundColor] = useState<string>("#D32D441A");
   const [filteredChartData, setFilteredChartData] = useState<any>(null);
-  const [topicName, setTopicName] = useState<{ id: string; name: string }[]>(
-    [],
-  );
-
-  // const topicNames = useSelector((state: RootState) => state.quiz.topicNames);
+  const [topicName, setTopicName] = useState<string[]>([]);
 
   const {
     data: studentTests,
@@ -92,7 +318,7 @@ export default function Analysis() {
   const handleTestIdChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedName = event.target.value;
     setSelectedTestId(selectedName);
-    setSelectedTopicName(selectedName); // Set the selected topic name
+    setSelectedTopicName(selectedName);
   };
 
   const minDate = useMemo(() => {
@@ -114,13 +340,11 @@ export default function Analysis() {
   }, [flattenedTests]);
 
   useEffect(() => {
-    const fiveMinutesInMillis = 23 * 59 * 59 * 1000;
-
     if (selectedTopicName || (startDate && endDate)) {
       const filteredTests = flattenedTests
         .filter(
           (test) =>
-            !selectedTopicName || test.test.topic_id === selectedTopicName,
+            !selectedTopicName || test.test.topic_name === selectedTestId,
         )
         .filter(
           (item: { created_at: string | number | Date }) =>
@@ -128,6 +352,7 @@ export default function Analysis() {
             (!endDate || new Date(item.created_at) <= endDate),
         );
 
+      console.log({ filteredTests });
       const filteredChartData = {
         labels: filteredTests.map((item) =>
           new Date(item.created_at).toLocaleDateString(),
@@ -152,17 +377,32 @@ export default function Analysis() {
       // Reset filteredChartData when no filters are applied
       setFilteredChartData(null);
     }
-  }, [selectedTopicName, startDate, endDate, flattenedTests]);
+    console.log({ selectedTestId, selectedTopicName });
+  }, [selectedTopicName, startDate, endDate, flattenedTests, selectedTestId]);
 
   useEffect(() => {
     if (studentTests) {
-      const uniqueTopics = Array.from(
-        new Set(studentTests.map((test) => test.topic_name)),
-      ).map((topicName) => ({
-        id: topicName,
-        name: topicName,
-      }));
-      setTopicName(uniqueTopics);
+      const subjectFilter = studentTests.filter(
+        (test) => test.course_name === query.analysis,
+      );
+
+      const uniqueTopic = Array.from(
+        new Set(subjectFilter.map((test) => test.topic_name)),
+      );
+
+      setTopicName(uniqueTopic);
+
+      const uniqueTest = studentTests.filter((item) =>
+        topicName.some((topic) => topic === item.topic_name),
+      );
+
+      console.log({ uniqueTest });
+      // const uniqueTopics = Array.from(
+      //   new Set(studentTests.map((test) => test.course_name)),
+      // ).map((topicId) => {
+      //   const topic = studentTests.find((test) => test.course_name === topicId);
+      //   return { id: topicId, name: topic?.topic_name || topicId };
+      // });
     }
   }, [studentTests]);
 
@@ -192,8 +432,8 @@ export default function Analysis() {
             >
               <option value="">Select Topic</option>
               {topicName.map((name, index) => (
-                <option key={index} value={name.id}>
-                  {name.name}
+                <option key={index} value={name}>
+                  {name}
                 </option>
               ))}
             </select>
@@ -201,7 +441,6 @@ export default function Analysis() {
               selectsRange
               startDate={startDate ?? undefined}
               endDate={endDate ?? undefined}
-              // allowSameDay={false}
               onChange={(update) => {
                 setDateRange(update);
               }}
