6c7f48a7f4215259a8a07e6017854c99463fe68a
Lukman10a <abdulrauflukman9@gmail.com>
2024-07-22 21:32:06 +0100

    update institution page

--- src/components/dashboardComponents/allClassCard.tsx
+++ src/components/dashboardComponents/allClassCard.tsx
@@ -55,8 +55,11 @@ export default function AllClassCard() {
   ];
   return (
     <div className="space-y-3">
-      {SUBJECT.map((item) => (
-        <div className="flex items-center justify-between rounded-md bg-[#F8F9FB] p-4">
+      {SUBJECT.map((item, index) => (
+        <div
+          className="flex items-center justify-between rounded-md bg-[#F8F9FB] p-4"
+          key={index}
+        >
           <p>{item.title}</p>
           <div className="flex justify-between gap-4">
             <button className="rounded-md bg-[#0F02041A] p-2 px-4">
